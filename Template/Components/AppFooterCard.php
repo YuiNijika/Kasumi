5fc5dce3aa042de3efd7e4319f5f9de48e33bfe9
鼠子 <Tomori@miomoe.cn>
2025-03-04 04:44:37 +0800

    修复备案号显示问题

--- Template/Components/AppFooterCard.php
+++ Template/Components/AppFooterCard.php
@@ -12,7 +12,7 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
             :style="{ display: 'flex', alignItems: 'center', }">
             <a-typography-text>© <?php echo date("Y"); ?> <a-link href="<?php Get::SiteUrl() ?>"><?php echo Get::Options("title"); ?></a-link> 版权所有</a-typography-text>
         </span>
-        <?php if (empty(Get::Options('IcpCode', true))) {
+        <?php if (empty(Get::Options('IcpCode'))) {
             echo '<a-link href="https://github.com/ShuShuicu/Typecho-Kasumi-Theme" target="_blank">Kasumi</a-link>';
         } else { ?>
             <a-link href="https://beian.miit.gov.cn/" target="_blank"><?php Get::Options('IcpCode', true) ?></a-link>
