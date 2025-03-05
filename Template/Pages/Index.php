834dd1d471142dc05efce8a1d30d5051f3afc38e
鼠子 <Tomori@miomoe.cn>
2025-03-06 02:49:28 +0800

    新增轮播图

--- Template/Pages/Index.php
+++ Template/Pages/Index.php
@@ -2,6 +2,11 @@
 if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 ?>
 <div id="Index">
+    <?php 
+        if (Get::Options('CarouselSwitch') === 'open') {
+            Kasumi::Components('IndexCarousel');
+        }
+    ?>
     <a-row>
         <?php
         while ($this->next()) {
