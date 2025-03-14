18ddab7b483ba3b0f381804272ca9b1177f781b0
Tomoriゞ <Tomori@miomoe.cn>
2025-03-15 01:50:41 +0800

    新增搜索

--- Template/Components/AppHeadMenu.php
+++ Template/Components/AppHeadMenu.php
@@ -24,14 +24,23 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
             ?>
         </a>
         <div class="mdui-toolbar-spacer"></div>
-        <a href="javascript:;" class="mdui-btn mdui-btn-icon">
+        <a href="javascript:;" class="mdui-btn mdui-btn-icon" mdui-dialog="{target: '#search-dialog'}">
             <i class="mdui-icon material-icons">search</i>
         </a>
-        <a href="javascript:;" class="mdui-btn mdui-btn-icon">
-            <i class="mdui-icon material-icons">refresh</i>
-        </a>
-        <a href="javascript:;" class="mdui-btn mdui-btn-icon">
-            <i class="mdui-icon material-icons">more_vert</i>
-        </a>
+    </div>
+</div>
+
+<div class="mdui-dialog" id="search-dialog" style="border-radius: 5px;" role="dialog" aria-labelledby="search-dialog-title">
+    <div class="mdui-dialog-title" id="search-dialog-title">搜索文章 · <small>精彩近在咫尺！</small></div>
+    <div class="mdui-dialog-content">
+        <form method="post" action="<?php echo Get::Options('siteUrl'); ?>" role="search">
+            <div class="mdui-textfield">
+                <i class="mdui-icon material-icons" aria-hidden="true">search</i>
+                <input class="mdui-textfield-input" type="search" name="s" placeholder="输入关键词后按回车(Enter)..." aria-label="搜索文章" required>
+            </div>
+        </form>
+    </div>
+    <div class="mdui-dialog-actions">
+        <button class="mdui-btn mdui-ripple" mdui-dialog-cancel>关闭</button>
     </div>
 </div>
