a0919e0c4966c95e9ee9616838f538fe4682255a
Tomoriゞ <Tomori@miomoe.cn>
2025-03-15 01:43:32 +0800

    优化post content

--- Template/Components/PostContent.php
+++ Template/Components/PostContent.php
@@ -1,11 +1,16 @@
 <?php
 if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 ?>
-<a-card title="<?php GetPost::Title(); ?>">
-    <template #extra>
-        <?php Kasumi::Components('PostShare') ?>
-    </template>
-    <div class="Kasumi-typo">
-        <?php GetPost::Content(); ?>
-    </div>
-</a-card>
+<article>
+    <a-card
+        role="article"
+        aria-label="文章卡片"
+        title="<?php GetPost::Title(); ?>">
+        <template #extra>
+            <?php Kasumi::Components('PostShare') ?>
+        </template>
+        <div class="Kasumi-typo">
+            <?php GetPost::Content(); ?>
+        </div>
+    </a-card>
+</article>
