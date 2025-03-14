7068a9058db2056954bd80c122284628e8454666
Tomoriゞ <Tomori@miomoe.cn>
2025-03-15 00:50:33 +0800

    修改为vue语法

--- Template/Components/AppFooterCard.php
+++ Template/Components/AppFooterCard.php
@@ -1,25 +1,35 @@
 <?php
 if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 ?>
-<a-card>
-    <div
-        :style="{
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'space-between',
-            }">
-        <span
-            :style="{ display: 'flex', alignItems: 'center', }">
-            <a-typography-text>© <?php echo date("Y"); ?> <a-link href="<?php Get::SiteUrl() ?>"><?php echo Get::Options("title"); ?></a-link> 版权所有</a-typography-text>
-        </span>
-        <?php if (empty(Get::Options('IcpCode'))) {
-        ?>
-            <a-tooltip content="由Kasumi主题强力驱动">
-                <a-link href="https://github.com/ShuShuicu/Typecho-Kasumi-Theme" target="_blank">Kasumi</a-link>
-            </a-tooltip>
-        <?php
-        } else { ?>
-            <a-link href="https://beian.miit.gov.cn/" target="_blank"><?php Get::Options('IcpCode', true) ?></a-link>
-        <?php } ?>
-    </div>
-</a-card>
+<div id="footer"></div>
+<script>
+    const FooterApp = {
+        data() {
+            return {
+                year: new Date().getFullYear(),
+                siteTitle: "<?php echo Get::Options('title'); ?>",
+                siteUrl: "<?php echo Get::SiteUrl(); ?>",
+                icpCode: "<?php echo Get::Options('IcpCode'); ?>",
+            };
+        },
+        template: `
+            <a-card>
+                <div :style="{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }">
+                    <span :style="{ display: 'flex', alignItems: 'center' }">
+                        <a-typography-text>© {{ year }} <a-link :href="siteUrl">{{ siteTitle }}</a-link> 版权所有</a-typography-text>
+                    </span>
+                    <template v-if="!icpCode">
+                        <a-tooltip content="由Kasumi主题强力驱动">
+                            <a-link href="https://github.com/ShuShuicu/Typecho-Kasumi-Theme" target="_blank">Kasumi</a-link>
+                        </a-tooltip>
+                    </template>
+                    <template v-else>
+                        <a-link href="https://beian.miit.gov.cn/" target="_blank">{{ icpCode }}</a-link>
+                    </template>
+                </div>
+            </a-card>
+        `,
+    };
+
+    Vue.createApp(FooterApp).mount('#footer');
+</script>
