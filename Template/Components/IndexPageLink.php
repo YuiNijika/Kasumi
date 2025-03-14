56fe821dfd438f81351e821e82561ac2865a20da
Tomoriゞ <Tomori@miomoe.cn>
2025-03-15 03:06:34 +0800

    updata

--- Template/Components/IndexPageLink.php
+++ Template/Components/IndexPageLink.php
@@ -3,7 +3,7 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 ?>
 <div style="text-align: center;margin-top: 10px;">
     <a-space>
-        <?php $this->pageLink('<a-button status="warning">上一页</a-button>'); ?>
-        <?php $this->pageLink('<a-button status="danger">下一页</a-button>', 'next'); ?>
+        <?php Get::PageLink('<a-button status="warning">上一页</a-button>') ?>
+        <?php Get::PageLink('<a-button status="success">下一页</a-button>', 'next') ?>
     </a-space>
 </div>
