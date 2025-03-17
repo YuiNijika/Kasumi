a69b534b80769b6fe5e810a0fa9697c7cc865636
Tomoriゞ <Tomori@miomoe.cn>
2025-03-17 08:09:52 +0800

    新增水印

--- Template/Pages/Post.php
+++ Template/Pages/Post.php
@@ -5,7 +5,9 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
     <div id="PostContent">
         <article>
             <div v-html="PostBreadcrumb" style="margin-bottom: 10px;"></div>
-            <?php Kasumi::Components('PostContent') ?>
+            <a-watermark content="<?php Get::SiteName() ?>">
+                <?php Kasumi::Components('PostContent') ?>
+            </a-watermark>
             <?php Kasumi::Components('PostButton'); ?>
         </article>
     </div>
